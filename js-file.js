b16a9d8e7f87471518b960c5e393026814c19d1f
Austin Taylor <austinmtaylor14@gmail.com>
2023-08-14 16:54:44 -0700

    Added RGB cell back-ground color
    got rid of the hovered class and made a function that generates
    random rgb, then applied that to a mouseenter event listener
    to make the UX better

--- js-file.js
+++ js-file.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         gridContainer.appendChild(cell); 
 
         cell.addEventListener("mouseenter", function () {
-            cell.classList.add("hovered"); 
+            cell.style.backgroundColor = getRandomColor(); 
         }); 
     }
 
@@ -31,11 +31,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 gridContainer.appendChild(cell); 
 
                 cell.addEventListener("mouseenter", function () {
-                    cell.classList.add("hovered"); 
+                    cell.style.backgroundColor = getRandomColor(); 
                 }); 
             }; 
         }); 
     }); 
 }); 
 
-
+function getRandomColor () {
+    const red = Math.floor(Math.random() * 256); 
+    const green = Math.floor(Math.random() * 256); 
+    const blue = Math.floor(Math.random() * 256); 
+    return `rgb(${red}, ${green}, ${blue})`; 
+}
